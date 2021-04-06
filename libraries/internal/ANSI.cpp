44f7341b032f5ebb4e03e8a386b0d88699845c6e
CreatorSiSo <64036709+CreatorSiSo@users.noreply.github.com>
2021-04-06 19:15:44 +0200

    add default for ANSI color

--- libraries/internal/ANSI.cpp
+++ libraries/internal/ANSI.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <string>
 
-std::string ANSI(std::string inputText, std::string txtColor,
+std::string ANSI(std::string txt, std::string Color = "white",
                  std::string bgColor = "black") {
 
   std::string ANSIClear = "\033[0m";
-  std::string ANSIDefaultTxtCol = "\x1B[91m";
+  std::string ANSIDefaultCol = "\x1B[91m";
   std::string ANSIDefaultBgCol = "\x1B[31m";
 
-  std::string outputText(ANSIDefaultTxtCol + inputText + ANSIClear);
+  std::string outputText(ANSIDefaultCol + txt + ANSIClear);
 
   /* size_t index = 0;
 
